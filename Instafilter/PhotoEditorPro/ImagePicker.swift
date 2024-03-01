99bdf166b0af32bf634667f4e7964e5fd479f710
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-02-29 21:36:51 -0800

    Image picker (PHPickerViewController)

---
+++ Instafilter/PhotoEditorPro/ImagePicker.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoEditorPro2.swift
+//  Instafilter
+//
+//  Created by Sothesom on 10/12/1402.
+//
+// image picker
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+}

e73443071b555127e395db8f3cca39949075188a
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-02-29 19:26:17 -0800

    1&2/11 : About didSet and its bugs and .onChange

---
+++ Instafilter/ContentView1.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Sothesom on 10/12/1402.
+//
+// Hudson: Instafilter SwiftUI Tutorial
+// GPT : Instafilter در SwiftUI یک کتابخانه و ابزار است که برای ایجاد فیلترهای تصویری و ویرایش تصاویر در برنامه‌های اپلیکیشن SwiftUI استفاده می‌شود. این ابزار به شما امکان می‌دهد تا با استفاده از SwiftUI ویژگی‌های گوناگونی از جمله تصاویر، فیلترها، ویرایش تصاویر و افکت‌های ویژه را به تصاویر اضافه کنید. از Instafilter می‌توان برای ایجاد اپلیکیشن‌هایی مانند ویرایشگرهای تصویر، اپلیکیشن‌های مبتنی بر شبکه‌های اجتماعی و حتی اپلیکیشن‌های بازی استفاده کرد.
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
